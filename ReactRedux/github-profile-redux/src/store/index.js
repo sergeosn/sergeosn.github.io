0c5245afac9ab210155c84712bfc606e231659cb
Serhii.Shmelov <shmelov.serhii@pdffiller.team>
2018-10-08 14:34:40 +0300

    Remove GitHub profile Redux

--- ReactRedux/github-profile-redux/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import appReducer from './reducers';
-import thunk from 'redux-thunk';
-
-export const store = createStore(appReducer, applyMiddleware(thunk));
